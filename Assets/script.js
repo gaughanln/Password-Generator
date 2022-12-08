e7b2167edcd69a6cf635e755c1b615cce3a76a55
gaughanln <gaughanln@yahoo.com>
2022-12-08 17:56:17 -0600

    deleted DS_Store

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var characters = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%&ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordmin = 8;
+var passwordmax = 128;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+for (var i = 0, 
+
   passwordText.value = password;
+
+sos
 
 }
 
