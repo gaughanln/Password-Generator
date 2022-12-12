833cd0f65518169fa2464de295c13f745ac0b833
gaughanln <gaughanln@yahoo.com>
2022-12-11 22:44:38 -0600

    fixed formatting

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,5 @@
 // Assignment Code
 
-// what is this query selector for?
 var generateBtn = document.querySelector("#generate");
 console.log("#generate");
 
@@ -72,85 +71,81 @@ var confirmnumber;
 var confirmspecialchar;
 
 function writePassword() {
+  // length prompt
+  var length = prompt("Choose a length between 8-128 characters");
+  if (length < 8 || length > 128) {
+    writePassword();
+    alert("Password length is either too short or too long, try again");
+    prompt("Choose a length between 8-128 characters");
+  }
+  console.log(length);
 
-// length prompt
-var length = prompt("Choose a length between 8-128 characters");
-if (length < 8 || length > 128)
-{
-	writePassword()
-  alert("Password length is either too short or too long, try again");
-  prompt("Choose a length between 8-128 characters");
-}
-console.log(length);
+  // confirming password paramaters
+  var confirmlowercase = confirm(
+    "Click OK if you want to include lowecase letters in your password"
+  );
+  console.log(confirmlowercase);
 
-// confirming password paramaters
-var confirmlowercase = confirm(
-  "Click OK if you want to include lowecase letters in your password"
-);
-console.log(confirmlowercase)
+  var confirmuppercase = confirm(
+    "Click OK if you want to include uppercase letters in your password"
+  );
+  console.log(confirmuppercase);
 
-var confirmuppercase = confirm(
-  "Click OK if you want to include uppercase letters in your password"
-);
-console.log(confirmuppercase)
+  var confirmnumber = confirm(
+    "Click OK if you want to include numbers in your password"
+  );
+  console.log(confirmnumber);
 
-var confirmnumber = confirm(
-  "Click OK if you want to include numbers in your password"
-);
-console.log(confirmnumber)
+  var confirmspecialchar = confirm(
+    "Click OK if you want to include special characters in your password"
+  );
+  console.log(confirmspecialchar);
 
-var confirmspecialchar = confirm(
-  "Click OK if you want to include special characters in your password"
-);
-console.log(confirmspecialchar)
+  var passwordText = [];
 
-// need a console.log here
-var passwordText = [];
+  // lowercase
+  if (confirmlowercase) {
+    passwordText = passwordText.concat(lowercase);
+  }
+  // uppercase
+  if (confirmuppercase) {
+    passwordText = passwordText.concat(uppercase);
+  }
+  // number
+  if (confirmnumber) {
+    passwordText = passwordText.concat(number);
+  }
+  // special character
+  if (confirmspecialchar) {
+    passwordText = passwordText.concat(specialchar);
+  }
 
-// lowercase
-if (confirmlowercase) {
-  passwordText = passwordText.concat(lowercase);
-}
+  // If no choices are made
+  if (
+    !confirmlowercase &&
+    !confirmuppercase &&
+    !confirmnumber &&
+    !confirmspecialchar
+  ) {
+    alert("You must choose a criteria, try again");
+    writePassword();
+  }
 
-// uppercase
-if (confirmuppercase) {
-  passwordText = passwordText.concat(uppercase);
-}
+  // setting length of password
+  var newPassword = "";
+  for (var i = 0; i < length; i++) {
+    var password =
+      passwordText[Math.floor(Math.random() * passwordText.length)];
+    newPassword += password;
+  }
 
-// number
-if (confirmnumber) {
-  passwordText = passwordText.concat(number);
-}
-
-// special character
-if (confirmspecialchar) {
-  passwordText = passwordText.concat(specialchar);
-}
-// console.log(passwordText);
-
-// If no choices are made
-if 
-(!confirmlowercase && !confirmuppercase && !confirmnumber && !confirmspecialchar) {
-	alert("You must choose a criteria, try again");
-  writePassword();
-}
-
-
-var newPassword = "";
-for (var i = 0; i < length; i++) {
-  var password = passwordText[Math.floor(Math.random() * passwordText.length)];
-  newPassword += password;
-}
-
-
-var password = newPassword;
-var passwordText = document.querySelector("#password");
+  var password = newPassword;
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-return passwordText;
+  return passwordText;
 }
-
 
 // event listener to generate button
 generateBtn.addEventListener("click", writePassword);
